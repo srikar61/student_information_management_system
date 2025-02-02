30d2fdcb2820b2e02fbb2553f975863f4bdaa0f8
srikar <srikar1233k@yahoo.com>
2025-02-02 11:57:43 +0530

    Initial Commit

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Admin = require('../models/Admin');
 
 
-const router = express.Router();
+const router = express.Router();//
 
 // Admin Signup
 router.post('/signup', async (req, res) => {
